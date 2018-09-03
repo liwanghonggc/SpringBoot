0ac0e769fd011cabf317b8068c94d8a1a3985b3b
lwh <2391519868@qq.com>
2018-09-03 14:59:51 +0800

    添加部分

---
+++ src/main/java/com/lwh/springboot/chp2/bean/HelloService.java
@@ -0,0 +1,7 @@
+package com.lwh.springboot.chp2.bean;
+
+/**
+ * 测试@@ImportResource
+ */
+public class HelloService {
+}
--- src/main/java/com/lwh/springboot/chp2/bean/Person.java
+++ src/main/java/com/lwh/springboot/chp2/bean/Person.java
@@ -2,6 +2,8 @@ package com.lwh.springboot.chp2.bean;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.List;
@@ -10,8 +12,13 @@ import java.util.Map;
 /**
  * 将配置文件中配置的每个属性的值,映射到这个组件中
  * @ConfigurationProperties 告诉SpringBoot将本类中所有属性的和配置文件中的值进行绑定
+ *                          prefix = "person" 配置文件中哪个下面的所有属性进行一一映射
+ * 只有这个组件是容器中的组件,容器才能提供@ConfigurationProperties功能
+ * 默认从全局配置文件中获取值
  */
+@Component
 @ConfigurationProperties(prefix = "person")
+@PropertySource(value = {"classpath:person.properties"})
 @Data
 public class Person {
 
@@ -28,6 +35,4 @@ public class Person {
     private List<Object> list;
 
     private Dog dog;
-
-
 }
