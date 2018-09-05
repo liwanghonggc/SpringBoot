def165cc20fbbb54266fe19fa41e959c5ccb50ef
lwh <2391519868@qq.com>
2018-09-05 15:36:57 +0800

    SpringBoot的CRUD实验

--- src/main/java/com/lwh/springboot/chp4/controller/LoginController.java
+++ src/main/java/com/lwh/springboot/chp4/controller/LoginController.java
@@ -16,6 +16,7 @@ public class LoginController {
                         @RequestParam("password") String password,
                         Map<String, Object> map, HttpSession session){
         if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+
             /** 登录成功,防止表单重复提交,可以重定向到主页 **/
             session.setAttribute("loginUserName", username);
             return "redirect:/main.html";
