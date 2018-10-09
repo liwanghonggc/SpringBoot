5b8b59ac22ac43a2be2667f1ea4d5888265fafcf
lwh <2391519868@qq.com>
2018-10-09 16:38:41 +0800

    SpringBoot的JDBC

--- src/main/java/com/lwh/springboot/chp6/JdbcController.java
+++ src/main/java/com/lwh/springboot/chp6/JdbcController.java
@@ -1,10 +1,31 @@
 package com.lwh.springboot.chp6;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author lwh
  * @date 2018-10-09
- * @desp
+ * @desp SpringBoot与JDBC整合测试
  */
+
+@Controller
 public class JdbcController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Map<String, Object> map(){
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from department");
+        return list.get(0);
+    }
 
 }
